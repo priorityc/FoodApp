b93670b06180c5fbb827462f8aa904db3e0e3590
Petya Dimitrova <petya83dimitrova@gmail.com>
2025-06-19 14:32:21 +0100

    added button and search component fixed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,10 +7,41 @@ import styles from "./search.module.css";
 const URL = `https://api.spoonacular.com/recipes/complexSearch`;
 const API_KEY = "2d9c01885882495eb47cc2575c918eec";
 
+
+// - inputValue = what the user is typing
+// - query = triggers the fetch logic via useEffect
+// - setQuery(inputValue) happens only on Search button click
+
 //accept foodData and setFoodData as props from App.jsx state
 export default function Search({ foodData, setFoodData }) {
   const [query, setQuery] = useState("pizza");
   //state for saving the food data
+  const [inputValue, setInputValue]=useState("")
+
+
+  async function fetchFood () {
+     //fetch the data and wait have to be fixed
+      //FIXED: Now your fetchFood() function will include the search term correctly
+    const res =await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+
+      //take the res and attach json, but we need to tell JS to wait until data is returned
+      const data = await res.json();
+      //in the console all the information is in obj
+      console.log(data.results);
+      //get the data array passed here as prop and set it to data.results
+      setFoodData(data.results);
+  }
+
+  useEffect(() => {
+    
+     fetchFood();
+  }, [query]); 
+
+  // Function to handle the search button
+  function handleSearchClick(e) {
+    e.preventDefault();
+    setQuery(inputValue)//triger fetch with useeffect
+  }
 
   //Syntax of the useeffect hoock Only when input changes we make a API call using React hook
   //passing callback func and dependency array
@@ -33,32 +64,25 @@ export default function Search({ foodData, setFoodData }) {
   //   }
   //   fetchFood();
   // }, []);
-
-  useEffect(() => {
-    //here we can use promise as well .then
-    async function fetchFood() {
-      //fetch the data and wait
-      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
-      //take the res and attach json, but we need to tell JS to wait until data is returned
-      const data = await res.json();
-      //in the console all the information is in obj
-      console.log(data.results);
-      //get the data array passed here as prop and set it to data.results
-      setFoodData(data.results);
-    } //function is not defined
-    fetchFood();
-  }, [query]); //execute demo func when type in
-
+  
   //Next how to create a state to save data from API results
   return (
+    <form onSubmit={handleSearchClick}>
     <div className={styles.searchContainer}>
       <input
         className={styles.input}
         type="text"
         // Take any input from the search bar and asign it to query
-        onChange={(e) => setQuery(e.target.value)}
-        value={query}
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
       />
+    {/* Added button that will fetch the search quiery */}
+      <button className={styles.button} type="submit">
+    Search
+  </button>
+
     </div>
+    </form>
   );
 }
+
